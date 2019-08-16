c15b6fa2da000d97b8eb05940422a0cc7c033c55
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2019-08-16 09:48:56 -0300

    Simple Create Form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -21,6 +22,20 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //Indica que vai ser uma ação de post
+        [ValidateAntiForgeryToken]//anotação para prevenir que a aplicação sofra ataques csrf, quando alguém aproveita a sua sessão de autenticação e envia dados maliciosos
+        public IActionResult Create(Seller seller) //essa operação recebe um objeto vendedor que veio na requisição, para receber este objeto e instanciar o vendedor, basta colocá-lo como parâmetro, é um recurso do EntityFramework
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));//redirecionar para o index
+
         }
     }
 }

dde8b599aa4e0412142c4e20dc73ce4b2b353079
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2019-08-18 22:36:50 -0300

    App locale, number and date formatting

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models
@@ -8,8 +9,15 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name = "Birth Date")] //anotation para definir o que vai aparecer nos labels, pois ele pega o nome como está na classe
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")] //0 indica a formatação do atributo
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }//com o isso framework detecta que vc quer guarda o id do dp, e se colocar como int, o int não pode ser nulo, e o dado vai ser guardado corretamente
